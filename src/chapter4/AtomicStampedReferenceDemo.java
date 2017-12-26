69dc319e6dc88a438f2c86801d362efada51b1bf
李锦 <lijin@webull.com>
2017-12-26 20:59:39 +0800

    add

--- src/chapter4/AtomicStampedReferenceDemo.java
+++ src/chapter4/AtomicStampedReferenceDemo.java
@@ -9,9 +9,9 @@ public class AtomicStampedReferenceDemo {
     static AtomicStampedReference<Integer> money = new AtomicStampedReference<Integer>(19, 0);
 
     public static void main(String args[]) {
-    	// 模拟多个线程同时持续更新后台数据库，为用户充值
+    	// 模拟3个线程同时持续更新后台数据库，为用户充值
         for (int i = 0; i < 3; i++) {
-            final int timestap = money.getStamp();
+            final int timestap = money.getStamp(); // 每个线程单独一份expectedStamp,移到while (true)中就不行了,因为只需要充值一次,我认为写在最外面可靠
             new Thread() {
                 public void run() {
                 	while(true){
@@ -37,16 +37,16 @@ public class AtomicStampedReferenceDemo {
             	// 模拟100次消费行为
                 for (int i = 0; i < 100; i++) {
                     while (true) {
-                        int timestap = money.getStamp();//预期时间戳
+                        int timestap = money.getStamp();//预期时间戳,一直都是在拿最新的
                         Integer m = money.getReference(); // 预期值
                         if (m > 10) {
                             System.out.println("金额大于10元");
                             if (money.compareAndSet(m, m - 10, timestap, timestap + 1)) {
-                                System.out.println("成功消费10元,余额:" + money.getReference() + "元");
+                                System.out.println("第"+i+"次成功消费10元,余额:" + money.getReference() + "元");
                                 break;// 当前消费行为结束,进行i++下一次消费行为
                             }
                         } else {
-//                            System.out.println("没有足够的金额");
+                            System.out.println("没有足够的金额");
                             break;
                         }
                     }
