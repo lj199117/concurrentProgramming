7f29d4166f674d44582fa82047b9d6a65f13ce78
李锦 <goldliclass@163.com>
2019-06-12 11:26:21 +0800

    aa

--- src/chapter3/threadpool/test2/CustomThreadPoolExecutorTest.java
+++ src/chapter3/threadpool/test2/CustomThreadPoolExecutorTest.java
@@ -30,17 +30,24 @@ public class CustomThreadPoolExecutorTest {
 		};
         CustomThreadPoolExecutor threadPoolExecutor = new CustomThreadPoolExecutor(1, 2, 1, TimeUnit.HOURS, taskQueue,threadFactory);
 
-        threadPoolExecutor.execute(()->{
+        /**
+         * 分析：
+         * 自定了一个ThreadPoolExecutor，核心线程数1，最大线程数2
+         * 然后提交了一个循环任务，主线程在2s后触发任务抛出异常，
+         * 而我们自定义的线程池可以遇到异常可以重新提交该任务
+         * 5s后触发关闭任务抛出异常
+         */
+        threadPoolExecutor.execute(() -> {
             //提交的是一个while(true)任务,正常运行时这类任务不会结束
             while (true) {
-                System.out.println("start processing");
+                System.out.println("start processing, thread=" + Thread.currentThread().getName());
                 try {
                     //模拟任务每次执行耗时1000ms
                     Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     //ignore
                 }
-                System.out.println("finish processing");
+                System.out.println("finish processing, thread=" + Thread.currentThread().getName());
 
                 if (stop) {
                     throw new RuntimeException("running encounter exception");
@@ -51,7 +58,7 @@ public class CustomThreadPoolExecutorTest {
         Thread.sleep(2000);
         //模拟 test- 线程 在执行任务过程中抛出异常
         stop = true;
-        Thread.sleep(1000);
+        Thread.sleep(5000);
         stop = false;
     }
 
